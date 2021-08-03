5bf320abe4a3c76786e104038c3586809b119bdb
Tolkachev Alexsey <atolkachev@sportmaster.ru>
2021-08-03 23:05:38 +0300

    подпроект common multiplatform

---
+++ common/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    java
+    kotlin("multiplatform")
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+kotlin {
+
+    js {
+        browser()
+    }
+    jvm {}
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-js"))
+            }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-jdk8"))
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit5"))
+            }
+        }
+    }
+}
+
+tasks.getByName<Test>("jvmTest") {
+    useJUnitPlatform()
+}
